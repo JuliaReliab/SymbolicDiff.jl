72208c19d6aa07fb9b0380304597f6a9e7a728c0
Hiroyuki Okamura <okamu@hiroshima-u.ac.jp>
2022-01-12 07:50:41 +0000

    v0.9.3

--- src/_env.jl
+++ src/_env.jl
@@ -6,3 +6,6 @@ const SymbolicEnv = Dict{Symbol,Any}
 const SymbolicCache = Dict{Union{AbstractSymbolic,Tuple{AbstractSymbolic,Symbol},Tuple{AbstractSymbolic,Tuple{Symbol,Symbol}}},Any}
 const globalenv = SymbolicEnv()
 
+function Base.setindex!(h::SymbolicEnv, k::SymbolicVariable{Tv}, x) where Tv
+    h[h.var] = x
+end
